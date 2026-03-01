8d7a38e002ef84695bfaac0c497fee981d209186
Mark Hudson <mark@hudsonsport.co.nz>
2026-03-02 07:16:41 +1300

    Fix Uint8Array type for pushManager.subscribe

--- app/hooks/usePushSubscription.ts
+++ app/hooks/usePushSubscription.ts
@@ -6,7 +6,12 @@ function urlBase64ToUint8Array(base64String: string): Uint8Array {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
   const rawData = atob(base64)
-  return Uint8Array.from([...rawData].map((char) => char.charCodeAt(0)))
+  const buffer = new ArrayBuffer(rawData.length)
+  const bytes = new Uint8Array(buffer)
+  for (let i = 0; i < rawData.length; i++) {
+    bytes[i] = rawData.charCodeAt(i)
+  }
+  return bytes
 }
 
 export function usePushSubscription() {
